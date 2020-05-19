39b2fb2cb404539d3143220fde80ce80ea9f9fc1
Federico Corradi <corradi.federico.91@gmail.com>
2020-05-19 17:22:45 +0100

    Removed log from pubblic/app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log( 'Client side javascript file is loaded' );
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const forecastText = document.querySelector('#forecast');
@@ -10,7 +8,7 @@ weatherForm.addEventListener( 'submit', (e) => {
   const location = search.value;
   locationText.textContent = 'Loading...';
   forecastText.textContent = '';
-  
+
   fetch( 'http://localhost:3000/weather?address=' + location ).then( ( response ) => {
 
     response.json().then( (data) => {
